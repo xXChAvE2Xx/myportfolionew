4eb55612c405f1d13158f0a8e6c5b94ee9e2a8d5
Raul Antonio Chavez Aguirre <raul25042000@hotmail.com>
2023-10-08 21:16:48 -0600

    se modifico porcentaje y se agrego clase para responsivoi

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class SkillsComponent {
 images: any[] = [
   { url: '../../assets/img/php.png', name: 'PHP', size: 200, progress: '80%', barColor: 'bg-info'},
-  { url: '../../assets/img/js.png', name: 'JS', size: 95, progress: '80%', barColor: 'bg-warning' },
-  { url: '../../assets/img/git.png', name: 'Git', size: 110, progress: '70%', barColor: 'bg-danger' },
-  { url: '../../assets/img/sql.png', name: 'Git', size: 110, progress: '70%', barColor: 'bg-success' },
+  { url: '../../assets/img/js.png', name: 'JS', size: 95, progress: '75%', barColor: 'bg-warning' },
+  { url: '../../assets/img/git.png', name: 'Git', size: 110, progress: '60%', barColor: 'bg-danger' },
+  { url: '../../assets/img/sql.png', name: 'SQLServer', size: 110, progress: '50%', barColor: 'bg-success' },
 ]
 }
